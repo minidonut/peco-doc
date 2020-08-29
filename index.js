8a1073ad8eb427bf52a82b7ed3733336a12ce819
minidonut <nycom13@gmail.com>
2020-08-29 17:56:47 +0900

    update

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const fs = require('fs');
+const path = require('path');
 const data = require('./database.json');
+const base = __dirname;
 const cmd = process.argv[2];
+const input = process.argv[3];
 
 if(cmd === 'add') {
   console.log(Object.keys(data).join('\n'));
 } else if(cmd === 'remove') {
-
+  console.log('remove: TBD');
 } else if(cmd === 'update') {
-  
+  console.log('update: TBD');
+} else if(cmd === 'get') {
+  const { url } = data[input.split(' ')[0].trim()];
+  console.log(url);
+} else if(cmd === 'generate') {
+  generate();
 }
 
 function generate() {
-  
+  const arr = Object.entries(data);
+  const maxKeyLength = arr.reduce((acc, [key]) => Math.max(acc, key.length) ,0);
+  const print = (key, desc) => `${key.padEnd(maxKeyLength + 2)}  ${desc}`;
+
+  fs.writeFileSync(
+    path.join(__dirname, 'generated'),
+    arr.map(([key, { description: desc }]) => print(key, desc)).join('\n')
+  );
 }
 
 
--- peco-doc.sh
+++ peco-doc.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-node ./index.js
+if [ ! -f "./generated" ]; then
+    node index.js generate
+fi
+
+if [ $1 = 'add' ]; then
+    node index.js add
+elif [ $1 = 'update' ]; then
+    node index.js update
+elif [ $1 = 'remove' ]; then
+    node index.js remove
+else  
+    key=$(cat generated | peco)
+    if [ -n "$key" ]; then
+    open $(node index.js get $key)
+    fi
+fi
 
